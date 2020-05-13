72db7aaa90549c3cec6ec98d1d2d8684052023f6
Barnaby Cotter <barnabycotter@gmail.com>
2020-05-13 16:17:13 +0100

    Add basic lambda -> s3 -> dynamo flow

--- Covid19_Antibody_uk/serverless/handler.js
+++ Covid19_Antibody_uk/serverless/handler.js
@@ -1,18 +1,62 @@
-'use strict';
+"use strict";
+const AWS = require("aws-sdk")
+const uuid = require("uuid")
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+// get reference to S3 client
+var s3 = new AWS.S3()
+var dynamo = new AWS.DynamoDB()
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+module.exports.upload = async (event) => {
+  
+  // Create our unique identifier for now (this will be supplied eventually)
+  const guid = uuid()
+
+  // Decode our base64 image from the body
+  const encodedImage = JSON.parse(event.body).rdt_image
+  const decodedImage = Buffer.from(encodedImage, "base64")
+  const filePath = `rdt-images/${guid}` + ".jpg"
+
+  // Prepare the S3 Insert
+  const s3Params = {
+    Body: decodedImage,
+    Bucket: process.env.UPLOAD_BUCKET,
+    Key: filePath,
+  }
+
+  try {
+    // Send to S3
+    const s3Upload = await s3.upload(s3Params).promise()
+    const { Location: image_url } = s3Upload
+    console.log(`Image uploaded to ${image_url}`)
+    let dynamoPut
+    // Once we have the items full url, prepare our put into dynamo
+    try {
+      const dynamoParams = {
+        TableName: process.env.DYNAMO_TABLE,
+        Item: {
+          guid: { S: guid },
+          image_url: { S: image_url }
+        }
+      }
+      
+      dynamoPut = await dynamo.putItem(dynamoParams).promise()
+      console.log("Dynamo record inserted:", dynamoPut)
+    } catch (error) {
+      //Dynamo insert has failed, undo our S3 insert as this will be detached data
+      await s3.deleteObject(params).promise()
+      return error
+    }
+    
+    // Return our s3 response and dynamo inserted item to the user
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        s3: s3Upload,
+        dynamo: dynamoParams.Item
+      })
+    }
+  } catch (error) { {
+      return error
+    }
+  }
+}
